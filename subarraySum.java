4a3298bfa4306de0a28036550a664d056b2869a6
hellooppl <49118684+hellooppl@users.noreply.github.com>
2019-09-01 09:13:18 +0545

    Add files via upload

---
+++ subarraySum.java
@@ -0,0 +1,52 @@
+package codingwars;
+
+import java.util.Scanner;
+
+public class subarraySum {
+
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int arrSize = sc.nextInt();
+		int sum = sc.nextInt();
+		
+		int[] lowest = new int[arrSize];
+		int j =0,  k =0;
+		int[] highest = new int[arrSize];
+		
+		for(int i =0; i<arrSize; i++) {
+		
+			int temp = sc.nextInt();
+			
+			if(temp<sum) 
+			{
+				lowest[j++] = temp;
+				
+			}
+			else 
+			{
+				highest[k++]  = temp;
+			}
+			
+
+		}
+		method2(lowest,sum,0);		
+		
+		
+		
+		
+	}
+		
+	
+	public static int method2(int [] lowest,int sum, int index) 
+	{
+		if(index == lowest.length) {
+			return lowest[index];
+		}
+	   if((method2(lowest,sum,index)+lowest[index])==12) {
+		   return index;
+	   }
+	   return lowest[index];
+	}
+	
+}
